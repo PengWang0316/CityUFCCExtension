7d46aa6a5785f8cac4866d23c66a296dbbdd903c
Kevin Wang <rocwang0316@gmail.com>
2019-08-18 15:40:31 -0700

    Added code to ask the content page to return html content

--- src/content.ts
+++ src/content.ts
@@ -8,6 +8,7 @@ const MARK_BG_COLOR = 'yellow';
 //   offsetTop: number;
 // }
 let lastElement;
+let javascriptSction;
 
 const emphasizeElement = (url: string) => {
   if (lastElement) lastElement.style.backgroundColor = '';
@@ -21,21 +22,23 @@ const emphasizeElement = (url: string) => {
 const clickAllSubSection = (element: HTMLElement) => {
   // wait for the ul element is loaded and click all li element under it.
   setTimeout(() => {
-    (element.parentNode.parentNode.lastChild as HTMLElement).querySelectorAll('h5').forEach((item: HTMLElement) => item.click());
+    element.querySelectorAll('h5').forEach((item: HTMLElement) => item.click());
   }, 500);
 };
 
-// Add a message listener
+// Add a message listener to response the click event in the popup page or asking content
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  emphasizeElement(request.url);
+  if (request.url) emphasizeElement(request.url);
+  else sendResponse({ html: javascriptSction.innerHTML });
 });
 
 const init = () => {
   // Click the JavaScript superblock and all sub sections under it
   document.querySelectorAll('h4').forEach((element: HTMLElement) => {
     if (element.innerText === JAVASCRIPT_SECTION_TITLE) {
-      clickAllSubSection(element);
       element.click();
+      javascriptSction = element.parentNode.parentNode.lastChild;
+      clickAllSubSection(javascriptSction);
     }
   });
 };
