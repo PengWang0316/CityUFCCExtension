c8b7f492e59925004ee3d84c69e8026ee2b3ec4b
Kevin Wang <rocwang0316@gmail.com>
2019-09-05 14:34:16 -0700

    Switched the output JSON formation order and add real data for moduel1

--- utils/pull-text.js
+++ utils/pull-text.js
@@ -6,6 +6,6 @@ let text = '';
 let result;
 
 // eslint-disable-next-line no-cond-assign
-while (result = regExp.exec(data)) text += `"${result[1]}": "${result[2]}"\n`;
+while (result = regExp.exec(data)) text += `"${result[2]}": "${result[1]}"\n`;
 
 console.log(text);
